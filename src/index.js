4bf80b67a6477dc7bd609e049a9a47ab70d837fb
timothy-spencer <timothy.spencer@gsa.gov>
2020-01-09 15:30:55 -0800

    first stab at getting data from our API.  Just does popup, chrome doesnt work

--- src/index.js
+++ src/index.js
@@ -1,12 +1,72 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class Scans extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      scantypes: [],
+      selected_scantype: null,
+    };
+  }
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+  componentWillMount() {
+    fetch(`https://site-scanning.app.cloud.gov/api/v1/scans/`)
+      .then(result => result.json())
+      .then(json => this.setState({scantypes: json}))
+  }
+
+  handleChange(event) {
+    this.setState({selected_scantype: event.target.value})
+  }
+
+  render() {
+    if (this.state.scantypes.length === 0) {
+      return null;
+    }
+
+    const scantypeselects = this.state.scantypes.map((scantype) => 
+      <option value="{scantype}">{scantype}</option>
+    );
+
+    return (
+      <div>
+        <div className="scans-chooser">
+          <label>
+            Scan type to view:
+            <select value={this.state.selected_scantype} onChange={this.handleChange}>
+              {scantypeselects}
+            </select>
+          </label>
+          <input type="submit" value="Submit" />
+        </div>
+        <div className="scans-viewer">
+          <Scansviewer />
+        </div>
+      </div>
+    );
+  }
+}
+
+class Scansviewer extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      page: null
+    }
+  }
+
+  render() {
+    return (
+      <p>Data lives here</p>
+    );
+  }
+}
+
+// ========================================
+
+ReactDOM.render(
+  <Scans />,
+  document.getElementById('root')
+);
